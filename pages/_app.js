3705f30dd82d504a7ad1dfd627217bec3e299d4c
abhigaprasad <abhigaprasadchitteth@gmail.com>
2024-08-18 15:31:09 +0530

    Convert useWindowWidth hook to ContextAPI. Declare the ContextAPI globally and access the isSmallerDevice property.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { WindowWidthProvider } from '../components/context/WindowWidthContext'; // Adjust the path as needed
 
 const App = ({ Component, pageProps }) => (
   <React.Fragment>
-    <Component {...pageProps} />
+    <WindowWidthProvider>
+      <Component {...pageProps} />
+    </WindowWidthProvider>
   </React.Fragment>
 );
 
